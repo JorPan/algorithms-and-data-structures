85c4396ad2f242894f56a52e7f2bd41eaef930f9
JorPan <jbpanasewicz@gmail.com>
2021-04-08 13:16:00 -0600

    bubble swap

---
+++ BubbleSort.js
@@ -0,0 +1,38 @@
+//BUBBLE SORT
+
+// compares 2 values at a time and moves the higher value to the end of the array.
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([4, 14, 212, 37, 35, 28, 8, 2]));
+
+function betterBubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(betterBubbleSort([4, 14, 212, 37, 35, 28, 8, 2]));
