a6ecf8fc5b9824abe58edabd5650ba659657d2c1
JorPan <jbpanasewicz@gmail.com>
2021-04-08 12:36:46 -0600

    naive string pattern search

--- NaiveStringSearch.js
+++ NaiveStringSearch.js
@@ -1,9 +1,20 @@
+//NAIVE STRING SEARCH
+
+// Check a long string to see how many times it contains a short string exactly.
+
 function naiveSearch(longString, shortString) {
+  let count = 0;
   for (let i = 0; i < longString.length; i++) {
     for (let j = 0; j < shortString.length; j++) {
-      console.log(longString[i], shortString[j]);
+      if (shortString[j] !== longString[i + j]) {
+        break;
+      }
+      if (j === shortString.length - 1) {
+        count++;
+      }
     }
   }
+  return count;
 }
 
-console.log(naiveSearch("lorie lolled", "lol"));
+console.log(naiveSearch("lorie loled", "lo"));
