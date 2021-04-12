fe973446c597321a8f4befa4744f9ae338ab6fd4
JorPan <jbpanasewicz@gmail.com>
2021-04-12 11:09:48 -0600

    merge sort using recursion

--- MergeSort.js
+++ MergeSort.js
@@ -1,5 +1,6 @@
 // MERGE SORT
 
+// STEP ONE
 // MERGE two SORTED arrays so that they return one SORTED array.
 
 function merge(arr1, arr2) {
@@ -29,3 +30,17 @@ function merge(arr1, arr2) {
 }
 
 console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+
+// STEP TWO - USES RECURSION
+// Break up arrays into halves until you ave arrays that are empty or have ONE element
+// Once you have smaller sorted arrays, merge those with other sorted arrays, and return the one SORTED array as results
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let midpoint = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, midpoint));
+  let right = mergeSort(arr.slice(midpoint));
+  return merge(left, right);
+}
+
+console.log(mergeSort([1, 7, 16, 2, 6, 99, 3]));
