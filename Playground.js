de6983853e0d3f0b99744594a84852c9e67b9ca0
JorPan <jbpanasewicz@gmail.com>
2021-05-03 13:00:30 -0600

    playground

---
+++ Playground.js
@@ -0,0 +1,17 @@
+function movieLengths(array, flightTime) {
+  let count = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let diff = flightTime - array[i];
+
+    if (count[diff]) {
+      return true;
+    }
+
+    count[array[i]] = i;
+  }
+
+  return false;
+}
+
+console.log(movieLengths([90, 60, 40], 120));
